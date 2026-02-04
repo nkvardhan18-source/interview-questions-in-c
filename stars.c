dfe7eb5a1d9bb751b79de50a488fdc2fdb6c3344
nkvardhan18-source <nkvardhan18@gmail.com>
2026-02-03 21:40:59 -0800

    Add files via upload

---
+++ stars.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+void main ()
+{
+    int rows;
+    printf ("enter no of rows\n");
+    scanf ("%d",&rows);
+    for (int i =1;i<=rows;i++)
+    {
+        printf("* \n");
+    }
+}
